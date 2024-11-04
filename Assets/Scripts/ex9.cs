f1ec66122b8c036bfc5027947fe19f293e025327
CAUA AUGUSTO DE OLIVEIRA <43130239847@info.local>
2024-11-04 17:12:15 -0300

    Ex. 9

--- Assets/Scripts/ex9.cs
+++ Assets/Scripts/ex9.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class ex9 : MonoBehaviour
 {
+
+    [SerializeField] int A;
+    [SerializeField] int B;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (A > B)
+        {
+            print("O time A foi o vencedor!");
+        }
+
+        else if (B > A)
+        {
+            print("O time B foi o vencedor!");
+        }
+
+        else if (A == B) {
+            print("Houve um empate entre os times!");
+
+        }
+
+        else if (A == B && A + B == 6)
+        {
+            print("Houve um empate EMOCIONANTE entre os times!");
+        }
+
+
     }
 
     // Update is called once per frame
