03d2cbbdc47ea8591ea3264c0f8f20a1357a3f40
CAUA AUGUSTO DE OLIVEIRA <43130239847@info.local>
2024-11-08 16:51:10 -0300

    Ex 6

---
+++ Assets/Scripts/Repetição/rep6.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rep6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        for (int i = 1; i <= 1000; i++)
+        {
+
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                print("Fizz Buzz");
+            }
+
+            else if (i % 3 == 0)
+            {
+                print("Fizz");
+            }
+
+            else if (i % 5 == 0)
+            {
+                print("Buzz");
+            }
+
+            else
+            {
+                print(i);
+            }
+
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
